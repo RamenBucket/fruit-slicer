325baca11078b57b90ad9a9e809a1ac1ea0c9571
Ricky h <1216173957@qq.com>
2020-11-07 11:20:58 +0800

    minor edit
    a function for reading txt...?

--- rhythmgame.py
+++ rhythmgame.py
@@ -3,4 +3,18 @@ print("Daniel Was here")
 print("Andrew changed this")
 print('Ricky was here')
 
-beatmap = open("beatmap.txt", "r")
+#beatmap = open("beatmap.txt", "r")
+
+def readfile(path):
+    with open(path,'rt') as f:
+        return f.read()
+
+def filetoList(file):
+    path = file
+    content = readfile(path)
+    result = []
+    for line in content.splitlines():
+        result.append(line.split(','))
+    return result
+
+print(filetoList('beatmap.txt'))
