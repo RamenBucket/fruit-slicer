41cb0bc07dcdb283e3674fa4e34deb32e2c65a7d
Daniel Yi <yidaniel12@gmail.com>
2020-11-07 11:33:37 -0500

    test

--- slicing.py
+++ slicing.py
@@ -1,4 +1,5 @@
 from cmu_112_graphics import *
+from clockwiseOrder import clockwiseOrder
 
 def appStarted(app):
     app.polygonList = [(50,50),(100,50),(100,100),(50,100)]
@@ -25,17 +26,22 @@ def calculateSlicePolygons(app):
     topPolygonList = []
     bottomPolygonList = []
 
-    topPolygonList.append(extendInDirection(app,x0,y0,dx,dy,1))
-    topPolygonList.append(extendInDirection(app,x0,y0,dx,dy,-1))
-    bottomPolygonList.append(extendInDirection(app,x0,y0,dx,dy,1))
-    bottomPolygonList.append(extendInDirection(app,x0,y0,dx,dy,-1))
-
     if dx == 0:
+        topPolygonList.append(extendInDirection(app,x0,y0,dx,dy,1))
+        topPolygonList.append(extendInDirection(app,x0,y0,dx,dy,-1))
+        bottomPolygonList.append(extendInDirection(app,x0,y0,dx,dy,1))
+        bottomPolygonList.append(extendInDirection(app,x0,y0,dx,dy,-1))
+
         topPolygonList.append((0,0))
         topPolygonList.append((0,app.height))
         bottomPolygonList.append((app.width,0))
         bottomPolygonList.append((app.width,app.height))
     elif dy == 0:
+        topPolygonList.append(extendInDirection(app,x0,y0,dx,dy,1))
+        topPolygonList.append(extendInDirection(app,x0,y0,dx,dy,-1))
+        bottomPolygonList.append(extendInDirection(app,x0,y0,dx,dy,1))
+        bottomPolygonList.append(extendInDirection(app,x0,y0,dx,dy,-1))
+
         topPolygonList.append((0,0))
         topPolygonList.append((app.width,0))
         bottomPolygonList.append((0,app.height))
@@ -43,21 +49,47 @@ def calculateSlicePolygons(app):
     else:
         slope = (dy/dx)
         intercept = (y0) - (slope*x0)
-        for xEnd,yEnd in [(0,0),(app.width,0),(app.width,app.height),
+        topPolygonList.extend(getIntercepts(app,slope,intercept))
+        bottomPolygonList.extend(getIntercepts(app,slope,intercept))
+        for xEnd,yEnd in [(0,0),(app.width,0),
+                          (app.width,app.height),
                           (0,app.height)]:
             # check if the point is above line
             if yEnd>slope*xEnd+intercept:
                 bottomPolygonList.append((xEnd,yEnd))
             else:
                 topPolygonList.append((xEnd,yEnd))
-                
-    return topPolygonList, bottomPolygonList
+    
+    print(f"top: {topPolygonList}")
+    print(f"topc: {clockwiseOrder(topPolygonList)}")
+    print(f"bottom: {bottomPolygonList}")
+    print(f"bottomc: {clockwiseOrder(bottomPolygonList)}")
+    return clockwiseOrder(topPolygonList), clockwiseOrder(bottomPolygonList)
 
 def extendInDirection(app,x,y,dx,dy,direction):
     while (x<=app.height and y<=app.width and x>=0 and y>=0):
         x+=dx*direction
         y+=dy*direction
     return (x,y)
+
+def getIntercepts(app,slope,intercept):
+    x0=1
+    y0=1
+    x1=app.width
+    y1=app.height
+
+    pt1 = (x0, slope*x0+intercept)
+    pt2 = ((y0-intercept)/slope , y0)
+    pt3 = (x1, slope*x1+intercept)
+    pt4 = ((y1-intercept)/slope , y1)
+
+    result = []
+
+    for x,y in [pt1,pt2,pt3,pt4]:
+        if x>=0 and y>=0 and x<=app.width and y<=app.height:
+            result.append((x,y))
+
+    return result
 
 def redrawAll(app, canvas):
     sliceTop = []
