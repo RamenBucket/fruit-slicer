e08897d758d59793e0043d556e16808abe197f50
Daniel Yi <yidaniel12@gmail.com>
2020-11-06 23:23:20 -0500

    Update slicing.py

---
+++ slicing.py
@@ -0,0 +1,12 @@
+from cmu_112_graphics import *
+
+def appStarted(app):
+    pass
+
+def mousePressed(app, event):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+runapp(width=512, height=512)
