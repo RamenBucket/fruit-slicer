b4e23b5485baf4260adb1659cf539762a1d068c9
andrewwchong <andrewwchong@gmail.com>
2020-11-08 04:04:07 +0800

    clockwise

---
+++ orderClockwise.py
@@ -0,0 +1,23 @@
+import math
+
+def orderClockwise(points): #jarvis
+    if len(points<3): return None # check actual polygon
+    Hull = list()
+    pointOnHull = points[0]  #leftmost rn
+    for point in points:
+        if(point[0] < pointOnHull[0]):
+            pointOnHull = point
+    #Hull.append(pointOnHull)
+
+    i = 0
+
+    while(not the same rn):
+        Hull.append(pointOnHull)
+        endpoint = points[0]
+        for point in points:
+
+
+
+
+        
+
