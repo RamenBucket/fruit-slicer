d0679af8333dc420297516b47373218bdc4948e6
andrewwchong <andrewwchong@gmail.com>
2020-11-08 05:33:06 +0800

    game file

---
+++ game.py
@@ -0,0 +1,52 @@
+
+
+import orderClockwise
+import centroid
+import Fruit
+import drawblade
+import slicing
+
+
+
+def appStarted(app):
+    pass
+
+   
+
+def mousePressed(app, event):
+    pass
+    
+
+
+def keyPressed(app, event):
+    pass
+
+def mouseReleased(app, event):
+    pass
+
+def mouseDragged(app,event):
+    pass
+
+def timerFired(app):
+    app.timerDelay = 20
+    doStep(app)       
+            
+
+def doStep(app):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+
+
+
+runApp(width=512, height=512)
+
+
+    
+
+
+
+
+
